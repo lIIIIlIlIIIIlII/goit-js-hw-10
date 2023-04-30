895bdc296a3a38107ca4c0116f00f51e50e8cef1
lIIIIlIlIIIIlII <118550128+lIIIIlIlIIIIlII@users.noreply.github.com>
2023-04-30 20:06:05 +0200

    3
    3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+// Получить ссылки на элементы DOM
+const countriesList = document.querySelector('.countries-list');
+const searchInput = document.querySelector('.search-input');
+
+// Создать функцию для отображения информации о странах на странице
+function displayCountries(countries) {
+  const html = countries
+    .map(
+      country => `
+        <div class="country">
+          <img class="flag" src="${country.flags.png}" alt="${country.name.common} Flag">
+          <h2 class="name">${country.name.common}</h2>
+          <p class="population">Population: ${country.population.toLocaleString()}</p>
+          <p class="region">Region: ${country.region}</p>
+          <p class="capital">Capital: ${country.capital}</p>
+        </div>
+      `
+    )
+    .join('');
+  countriesList.innerHTML = html;
+}
+
+// Создать функцию для фильтрации стран по введенному пользователем тексту
+function searchCountries(countries, searchText) {
+  return countries.filter(country => country.name.common.toLowerCase().includes(searchText.toLowerCase()));
+}
+
+// Создать функцию для обработки событий ввода текста в поле поиска
+function handleSearch() {
+  const searchText = searchInput.value.trim();
+  const filteredCountries = searchCountries(countries, searchText);
+  if (filteredCountries.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+  if (filteredCountries.length === 0) {
+    Notify.failure('No matches found. Please enter a valid name.');
+    return;
+  }
+  displayCountries(filteredCountries);
+}
+
+// Инициализировать приложение
+let countries = [];
+
+fetchCountries().then(data => {
+  countries = data;
+  displayCountries(countries);
+});
+
+searchInput.addEventListener('input', debounce(handleSearch, 500));
